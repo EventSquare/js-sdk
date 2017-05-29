ab3d16f0fb3a892a9994ffe72192fb2abddfcf7f
Willem Staels <willem@epicbrands.be>
2017-05-29 18:29:38 +0200

    Add methods to update cart items quantity

--- tests/connect.js
+++ tests/connect.js
@@ -3,15 +3,18 @@ require('dotenv').config()
 
 EventSquare.init({
     apikey: process.env.API_KEY,
+});
+
+EventSquare.store({
     uri: process.env.STORE_URI,
     lang: 'en',
     agent: 'EventSquare Agent',
     ip: '1.1.1.1',
-    cart: '1cb03430-fb1b-4add-8f69-369a1702ce91',
+    cart: 'bfa74d2a-eb2c-4675-b28a-7f2bc4e5fa3b',
 },function(err,store){
     if(err){
         console.error(err);
         return;
     }
-    console.log('Succesfully connected');
+    console.log('Succesfully connected (Cart: ' + store.cart.cartid + ')');
 });
