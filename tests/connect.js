7ce35d58411876b4fe4940f3f9d7e34d52f3d328
Willem Staels <willem@epicbrands.be>
2017-05-31 09:25:03 +0200

    Change store method

--- tests/connect.js
+++ tests/connect.js
@@ -8,9 +8,8 @@ EventSquare.init({
 EventSquare.useEndpoint(process.env.API_ENDPOINT);
 EventSquare.useCheckout(process.env.CHECKOUT_URL);
 
-EventSquare.store({
-    uri: process.env.STORE_URI,
-    lang: 'en',
+EventSquare.store(process.env.STORE_URI,{
+    language: 'en',
     agent: 'EventSquare Agent',
     ip: '1.1.1.1',
     //cart: 'bfa74d2a-eb2c-4675-b28a-7f2bc4e5fa3b',
@@ -20,5 +19,4 @@ EventSquare.store({
         return;
     }
     console.log('Succesfully connected (Cart: ' + store.cart.cartid + ')');
-    console.log(EventSquare.getFastlaneUrl());
 });
