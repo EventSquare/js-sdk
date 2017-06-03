1deedf4afe6bcb344d58cec9e50d54e51734e213
Willem Staels <willem@epicbrands.be>
2017-06-03 12:15:04 +0200

    Update modify type to allow timeslots

--- tests/connect.js
+++ tests/connect.js
@@ -11,7 +11,7 @@ EventSquare.useCheckout(process.env.CHECKOUT_URL);
 EventSquare.store("kerstmagie/wea2nmitludc",{
     language: 'nl',
     ip: '1.1.1.1',
-    //cart: 'bfa74d2a-eb2c-4675-b28a-7f2bc4e5fa3b',
+    //cart: '98d326f3-4b72-4677-83a2-ddbd2a49d56c', //Force reuse of the same cart
 },function(err,store){
     if(err){
         console.error(err);
@@ -19,13 +19,49 @@ EventSquare.store("kerstmagie/wea2nmitludc",{
     }
     console.log('Succesfully connected and created a cart (' + store.cart.cartid + ')');
 
+    //Get available types
+    var store_types = (EventSquare.helpers.list(store));
+
+    console.log('There are ' + store_types.length + ' types available in this store.');
+
+    if(store_types.length){
+
+        console.log('Trying to add the first type (' + store_types[0].id + ') to the cart');
+
+        //Add items to cart
+
+        var request = {
+            quantity: 1
+        }
+
+        EventSquare.updateType(store.cart.cartid,store_types[0].id ,request,function(err,cart){
+
+            if(err){
+                console.error(err);
+                return;
+            }
+            console.log('Succesfully added to the cart');
+            loadCart(store);
+
+        });
+
+    }
+
+});
+
+function loadCart(store){
+
     EventSquare.cart(store.cart.cartid,function(err,cart){
 
         if(err){
             console.error(err);
             return;
         }
+
         console.log('Succesfully requested cart details');
+
     });
 
-});
+
+
+}
